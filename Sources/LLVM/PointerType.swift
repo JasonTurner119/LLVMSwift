0875805084569de2787050ce80d43089403a4ff5
JasonTurner <jasonturner119@icloud.com>
2024-09-30 20:39:45 -0700

    Added implementations for the deprecated `PointerType.init` and `PointerType.toVoid`. Added a stub with an error for `PointerType.pointee`.

--- Sources/LLVM/PointerType.swift
+++ Sources/LLVM/PointerType.swift
@@ -12,11 +12,12 @@ import cllvm
 ///
 /// Note that LLVM no longer uses typed pointers and instead uses [Opaque Pointers]( https://llvm.org/docs/OpaquePointers.html ).
 public struct PointerType: IRType {
+  
   internal let llvm: LLVMTypeRef
   
   /// Retrieves the address space where the pointed-to object resides.
   public let addressSpace: AddressSpace
-
+  
   /// Creates a `PointerType` in an optional context and address space.
   ///
   /// - parameter context: The context to create this type in. Default is `.global`.
@@ -26,11 +27,37 @@ public struct PointerType: IRType {
     llvm = LLVMPointerTypeInContext(context.llvm, UInt32(addressSpace.rawValue))
     self.addressSpace = addressSpace
   }
-
+  
   /// Retrieves the underlying LLVM type object.
   public func asLLVM() -> LLVMTypeRef {
     return llvm
   }
+  
+}
+
+// MARK: Deprecations
+
+extension PointerType {
+  
+  /// Creates a `PointerType` from a pointee type and an optional address space.
+  /// Creates a `PointerType` in an optional context and address space.
+  ///
+  /// - parameter pointee: The type of the pointed-to object.
+  /// - parameter context: The context to create this type in. Default is `.global`.
+  /// - parameter addressSpace: The optional address space where the pointed-to
+  ///   object resides.
+  /// - note: The context of this type is taken from it's pointee
+  @available(*, deprecated, message: "Use PointerType.init(in:addressSpace:) instead.")
+  public init(pointee: IRType, addressSpace: AddressSpace = .zero) {
+    self.init(in: pointee.context, addressSpace: addressSpace)
+  }
+  
+  @available(*, deprecated, message: "Use PointerType.init(in:addressSpace:) instead.")
+  public static let toVoid = PointerType()
+  
+  @available(*, deprecated, message: "Typed pointers are no longer supported by LLVM, the pointee type should be stored elsewhere.")
+  public static var pointee: Never { fatalError("PointerType.pointee is not supported because all pointers are opaque.") }
+  
 }
 
 extension PointerType: Equatable {
